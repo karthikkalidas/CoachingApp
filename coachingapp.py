9ca4b075c64b08bcbdb65fa341a6ca70550cfc0e
karthikkalidas <karthik.kalidas@gmail.com>
2016-07-07 22:23:05 +0530

    All functions created. Debugging to be done

--- coachingapp.py
+++ coachingapp.py
@@ -5,44 +5,71 @@ import sqlite3
 #Connection and Cursor
 conn = sqlite3.connect('coaching.db')
 c = conn.cursor()
-StudentList=[]
+Marks_HistoryList=[]
 
-class Student:
-
-	def __init__(self, s_name, s_batch, s_marks, s_rank):
-		self.s_name=s_name
-		self.s_batch=s_batch
-		self.s_marks=s_marks
-		self.s_rank=s_rank
 
 def create_table():
-	c.execute("CREATE TABLE IF NOT EXISTS coaching(Name TEXT, Batch TEXT, Marks TEXT, Rank REAL)")		
+	c.execute("CREATE TABLE IF NOT EXISTS coaching(Name TEXT, Batch TEXT, Marks_History TEXT, Total_Marks REAL, Rank REAL)")		
+
+def marks_list_generate(s_name):
+	c.execute('SELECT Marks_History FROM coaching WHERE Name = ?',(s_name))
+	data=c.fetchall()
+	Marks_HistoryList=str(data).split()
+	return Marks_HistoryList
+
+def Total_Marks_generate():
+	c.execute('SELECT Marks_History FROM coaching')
+	data = c.fetchall()      
+	for i in range(1,len(data)+1):
+		c.execute('SELECT Marks_History FROM coaching WHERE rowid = ?',i)
+		data=c.fetchall()
+		Marks_HistoryList=str(data).split()
+		total_marks=sum(Marks_HistoryList)
+		c.execute('UPDATE coaching SET Total_Marks = ? WHERE rowid = ?',(total_marks,i))   
+		conn.commit()
 
 def add_student():
 	s_name=input("Enter Student's Name : ")
-	s_marks=input("Enter Student's Marks : ")
-	c.execute("INSERT INTO coaching(Name, Marks1) VALUES (?, ?)",(s_name, s_marks))
+	c.execute("INSERT INTO coaching(Name) VALUES (?)",(s_name))
 	conn.commit()
+
+
+def update_marksheet():
+	s_name=input("Enter Student's Name : ")
+	c.execute('SELECT Name FROM coaching')
+	data = c.fetchall()
+	string_list = map(' '.join, data) 
+	NameList=str(''.join(string_list)).split()
+	while True:
+		if (NameList==[]):	
+			print("Please add a student first")
+			quit()
+		elif s_name not in NameList:
+			s_name=input("Enter a valid Student Name : ")
+		else:
+			break
+
+	s_marks=input("Enter Student's Latest Marks : ")
+	Marks_HistoryList=marks_list_generate(s_name)
+	Marks_HistoryList.append(s_marks)
+	s_marks_history = ' '.join(Marks_HistoryList)
+	c.execute('UPDATE coaching SET Marks_History = ? WHERE Name = ?',(s_marks_history, s_name))
+	conn.commit()
+	Total_Marks_generate()
 	rank_generate()
 	batch_generate()
-	c.execute('SELECT Batch FROM coaching WHERE Name = ?',(s_name))
-	s_batch =c.fetchall()
-	c.execute('SELECT Rank FROM coaching WHERE Name = ?',(s_name))
-	data=c.fetchall()
-	s_rank =str(data[0])
-	new_student=Student(s_name,s_batch,s_marks,s_rank)
-	StudentList.append(new_student)
 
+#On the basis of overall test marks
 def rank_generate():
-	c.execute('SELECT Marks1 FROM coaching ORDER BY Marks1 DESC')
+	c.execute('SELECT Total_Marks FROM coaching ORDER BY Total_Marks DESC')
 	data = c.fetchall()
 	for i in range(1,len(data)+1):
-		c.execute('UPDATE coaching SET Rank = ? WHERE Marks1 = ?',(i,str(data[i-1][0])))   
+		c.execute('UPDATE coaching SET Rank = ? WHERE Total_Marks = ?',(i,str(data[i-1][0])))   
 		conn.commit()
 
 #Batches of 3
 def batch_generate():
-	c.execute('SELECT Rank FROM coaching ORDER BY Marks1 DESC')
+	c.execute('SELECT Rank FROM coaching ORDER BY Total_Marks DESC')
 	data = c.fetchall()
 	batch_count=0
 	for i in range(1,len(data)+1):
@@ -60,49 +87,81 @@ def read_from_db():
 	data = c.fetchall()
 	for row in data:
 		print(row)
+	input("\n\nPRESS ENTER TO CONTINUE :")
 
 def latest_marks(s_name):
-	c.execute('SELECT * FROM coaching WHERE ID = (SELECT MAX(ID) FROM coaching) AND Name = ?',s_name)  
-	data = c.fetchall()
-	for row in data:
-		print(row)
+	Marks_HistoryList=marks_list_generate(s_name)
+	print("Marks : "+ Marks_HistoryList[-1])
 
 def studentbatch_view(s_name):
-	c.execute('SELECT Rank FROM coaching WHERE Name = ?',s_name)
+	c.execute('SELECT Batch FROM coaching WHERE Name = ?',s_name)
 	data = c.fetchall()
 	for row in data:
 		print(row)
 
-def new_test_marks():
-	testno=input("Enter Test Number : ")
-	c.execute('ALTER TABLE coaching ADD COLUMN ? TEXT','Marks'+testno)
-	conn.commit()
+def studentperf_view(s_name):
+	c.execute('SELECT Marks_History FROM coaching WHERE Name = ?',s_name)
+	data = c.fetchall()
+	for row in data:
+		print(row)
 
 def adminint():
 	os.system('clear')
-	func=int(input("Choose Any Number : \n\n(1) Add Student\n(2) View Database\n\n"))
+	# while True:
+	# 	try:
+	# 		func=int(input("Choose Any Number : \n\n(1) Add Student\n(2) View Database\n\n"))
+
+	# 	except ValueError:
+	# 		func=int(input("Choose Any Number : \n\n(1) Add Student\n(2) View Database\n\n"))
+
+	# 	else:
+	# 		break
+
+	func=int(input("Choose Any Number : \n\n(1) Update Marksheet\n(2) View Database\n(3) Add Student\n\n"))
 	os.system('clear')
 	if func == 1:
-		add_student()
+		update_marksheet()
 	elif func == 2:
 		read_from_db()
+	elif func == 3:
+		add_student()
 	else:
 		print("Your option doesn't make sense to me")
+	os.system('clear')
 
 def studentint():
 	os.system('clear')
 	s_name=input("Enter Your Name : ")
 	os.system('clear')
 	func=int(input("Choose Any Number : \n\n(1) View Latest Marks\n(2) View Batch\n(3) View Performance History\n"))
+
+	while True:
+		if isinstance(func, int):
+			break
+		else:
+			func=int(input("Choose A Valid Number : \n\n(1) View Latest Marks\n(2) View Batch\n(3) View Performance History\n"))
+
 	os.system('clear')
 	if func == 1:
 		latest_marks(s_name)
 	elif func == 2:
-		studentmarks_view(s_name)
+		studentbatch_view(s_name)
+	elif func==3:
+		studentperf_view(s_name)
 	else:
 		print("Your option doesn't make sense to me")
+	os.system('clear')
 
-
+#Main
 create_table()
-studentint()
-# studentint()
+os.system('clear')
+print("COACHING APP\n")
+while True:
+	inp=int(input("Choose a Number : \n\n1)ADMINISTRATOR \n2)STUDENT \n\n"))
+	if inp==1:
+		adminint()
+	elif inp==2:
+		studentint()
+	else:
+		os.system('clear')
+		inp=int(input("Choose a Number : \n\n1)ADMINISTRATOR \n2)STUDENT \n\n"))
